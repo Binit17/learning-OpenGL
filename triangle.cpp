6d9d6531e3aff25d6b57a11daa9ce0232e3d2213
Binit kc <077bct017.binit@pcampus.edu.np>
2023-07-08 21:54:35 +0545

    just made some changes in tringle

--- triangle.cpp
+++ triangle.cpp
@@ -1,4 +1,4 @@
-#include <glad/glad.h>"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -20,7 +20,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(0.1f, 0.9f, 0.4f, 1.0f);\n"
     "}\n\0";
 
 int main()
@@ -100,7 +100,7 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left  
+        -0.1f, -0.2f, 0.0f, // left  
          0.5f, -0.5f, 0.0f, // right 
          0.0f,  0.5f, 0.0f  // top   
     }; 
