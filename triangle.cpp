e588e42d1baaec28f35bf786afd510c374520cb4
Binit kc <077bct017.binit@pcampus.edu.np>
2023-07-19 22:13:02 +0545

    learned about shades and implemented some codes to give different colors to triangle at runtime

--- triangle.cpp
+++ triangle.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include<math.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -18,9 +19,10 @@ const char *vertexShaderSource = "#version 330 core\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "uniform vec4 ourColor;\n" //we set this variable in the OpenGL code.
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(0.1f, 0.9f, 0.5f, 1.0f);\n"
+    "   FragColor = ourColor;\n"
     "}\n\0";
 
 int main()
@@ -143,6 +145,15 @@ int main()
 
         // draw our first triangle
         glUseProgram(shaderProgram);
+
+        //update the uniform color
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue)/2.0f + 0.5f;
+        float redValue = sin(timeValue)/2.0f + 0.2f;
+        float blueValue = sin(timeValue)/2.0f + 0.4f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, redValue,greenValue,blueValue,1.0f);
+
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time 
